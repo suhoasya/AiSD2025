55b84a0e1b17b4e89d9303ffbc2e5f7ab513bde3
suhoasya <suho-asya@list.ru>
2026-02-08 22:49:25 +0500

    L0906_331test.cpp
    Добавила #include backtrack

--- L0906_331/L0906_331test.cpp
+++ L0906_331/L0906_331test.cpp
@@ -1,9 +1,7 @@
 #include "L0906_331.hpp"
 #include <iostream>
-void backtrack(int a[], int k, int n)
-{
-	std::cout<<k<<std::endl<<n;
-}
+#include "../L902_327/L902_327.hpp"
+
 int main()
 {
 	generate_subsets(5); //0 5

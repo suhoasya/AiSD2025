f280dad47131c358ff0eab3803586cffa80886ab
suhoasya <suho-asya@list.ru>
2026-02-08 22:42:52 +0500

    L0904_329.cpp
    Добавила #include backtrack

--- L0904_329/L0904_329.cpp
+++ L0904_329/L0904_329.cpp
@@ -3,9 +3,9 @@
 //ПМ-2025 Суханова Анастасия
 #include "L0904_329.hpp"
 #include <iostream>
+#include "../L902_327/L902_327.hpp"
 
 const int NMAX = 100;
-void backtrack(int a[], int k, int n);
 
 void generate_subsets(int n)
 {
